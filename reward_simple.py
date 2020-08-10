0a6fda762f0d85cd946dc03d491e43a1bba24b4c
Matthew Suntup <matthewsuntup@gmail.com>
2020-08-11 00:11:48 +1000

    Updated for bowtie track

--- reward_simple.py
+++ reward_simple.py
@@ -12,8 +12,10 @@ def reward_function(params):
     distance_from_center = params['distance_from_center']
     heading = params['heading']
     is_offtrack = params['is_offtrack']
+    progress = params['progress']
     speed = params['speed']
     steering_angle = params['steering_angle']
+    steps = params['steps']
     track_width = params['track_width']
     waypoints = params['waypoints']
     x = params['x']
@@ -29,9 +31,9 @@ def reward_function(params):
     
     # Give higher reward if the car is closer to centre line and vice versa
     # 0 if you're on edge of track, 1 if you're centre of track
-    reward = 1 - distance_from_center/(track_width/2)
+    reward = 1 - (distance_from_center/(track_width/2))**(1/2) + progress/steps
 
     if abs(steering_angle) < 10 and speed > 2:
-        reward += speed/max_speed
+        reward += (speed/max_speed)**2
       
     return float(reward)
