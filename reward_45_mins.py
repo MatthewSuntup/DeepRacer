78bcc6ffdc0f5ab695b6113b977ab7e6941c0cfb
georgiemarkham <georgiegeorgiee@gmail.com>
2020-08-13 21:29:57 +1000

    Added waypoints to 45 min file

--- reward_45_mins.py
+++ reward_45_mins.py
@@ -4,6 +4,8 @@ def reward_function(params):
     Example of rewarding the agent to follow center line
     '''
 
+    import math
+
     # Read input parameters
     all_wheels_on_track = params['all_wheels_on_track']
     track_width = params['track_width']
@@ -11,19 +13,15 @@ def reward_function(params):
     steering = abs(params['steering_angle']) # Only need the absolute steering angle
     progress = params['progress']
     steps = params['steps']
+    speed = params['speed']
+    waypoints = params['waypoints']
+    x = params['x']
+    y = params['y']
 
     # Calculate 3 markers that are at varying distances away from the center line
     marker_1 = 0.1 * track_width
     marker_2 = 0.25 * track_width
     marker_3 = 0.5 * track_width
-
-    # Give a very low reward by default
-    reward = 1e-3
-
-    # Give a high reward if no wheels go off the track and
-    # the agent is somewhere in between the track borders
-    if all_wheels_on_track and (0.5*track_width - distance_from_center) >= 0.05:
-        reward = 1.0
 
     # Give higher reward if the car is closer to center line and vice versa
     if distance_from_center <= marker_1:
@@ -43,5 +41,37 @@ def reward_function(params):
         reward *= 0.8
 
     reward *= (progress/steps)*2
+
+    ####################
+    future_step = 5
+
+    # Identify next waypoint and further waypoint
+    point_prev = waypoints[closest_waypoints[0]]
+    point_next = waypoints[closest_waypoints[1]]
+    point_future = waypoints[min(len(waypoints)-1,closest_waypoints[1]+future_step)]
+
+    # Calculate headings to waypoints
+    heading_current = math.degrees(math.atan2(point_prev[1]-point_next[1], point_prev[0] - point_next[0]))
+    heading_future = math.degrees(math.atan2(point_prev[1]-point_future[1], point_prev[0]-point_future[0]))
+
+    # Circular Heading Calculations
+    if heading_current > heading_future and heading_current - heading_future > 180:
+        heading_offset = 180-heading_current
+        heading_current = -180
+        heading_future += heading_offset
+
+    elif heading_future > heading_current and heading_future - heading_current > 180:
+        heading_offset = 180-heading_future
+        heading_future = -180
+        heading_current += heading_offset
+
+    # Calculate the difference between the headings
+    diff_heading = abs(heading_current-heading_future)
+
+    if diff_heading < 10 and speed > 2.5:
+        reward *= 1.5
+
+    if diff_heading > 10 and speed < 2.5:
+        reward *= 1.5
 
     return float(reward)
