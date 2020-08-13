074042005aef0190858314d141f95bc914cbb5b4
Matthew Suntup <matthewsuntup@gmail.com>
2020-08-14 02:09:52 +1000

    Updating speed control based on waypoint research

--- reward_simple.py
+++ reward_simple.py
@@ -6,11 +6,47 @@ def reward_function(params):
     import math
     import numpy as np
     
+    # Params
+    FUTURE_STEP = 7
+    MID_STEP = 4
+    TURN_THRESHOLD = 10     # degrees
+    DIST_THRESHOLD = 1.2    # metres
+
+    def identify_corner(waypoints, closest_waypoints, future_step):
+
+        # Identify next waypoint and further waypoint
+        point_prev = waypoints[closest_waypoints[0]]
+        point_next = waypoints[closest_waypoints[1]]
+        point_future = waypoints[min(len(waypoints)-1,closest_waypoints[1]+future_step)]
+
+        # Calculate headings to waypoints
+        heading_current = math.degrees(math.atan2(point_prev[1]-point_next[1], point_prev[0] - point_next[0]))
+        heading_future = math.degrees(math.atan2(point_prev[1]-point_future[1], point_prev[0]-point_future[0]))
+
+        # Circular Heading Calculations
+        if heading_current > heading_future and heading_current - heading_future > 180:
+            heading_offset = 180-heading_current
+            heading_current = -180
+            heading_future += heading_offset
+
+        elif heading_future > heading_current and heading_future - heading_current > 180:
+            heading_offset = 180-heading_future
+            heading_future = -180
+            heading_current += heading_offset
+
+        # Calculate the difference between the headings
+        diff_heading = abs(heading_current-heading_future)
+
+        # Calculate distance to waypoints
+        dist_future = np.linalg.norm([point_next[0]-point_future[0],point_next[1]-point_future[1]])  
+
+        return diff_heading, dist_future
+
+
     # Read input parameters
     all_wheels_on_track = params['all_wheels_on_track']
     closest_waypoints = params['closest_waypoints']
     distance_from_center = params['distance_from_center']
-    heading = params['heading']
     is_offtrack = params['is_offtrack']
     progress = params['progress']
     speed = params['speed']
@@ -18,11 +54,6 @@ def reward_function(params):
     steps = params['steps']
     track_width = params['track_width']
     waypoints = params['waypoints']
-    x = params['x']
-    y = params['y']
-    
-    # Params
-    max_speed = 4
 
     # Strongly discourage going off track
     if not all_wheels_on_track or is_offtrack:
@@ -33,10 +64,32 @@ def reward_function(params):
     # 0 if you're on edge of track, 1 if you're centre of track
     reward = 1 - (distance_from_center/(track_width/2))**(1/4) + progress/steps
 
-    if abs(steering_angle) < 10 and speed > 2:
+    diff_heading, dist_future = identify_corner(waypoints, i, FUTURE_STEP)
+
+    go_fast = True
+
+    if diff_heading < TURN_THRESHOLD:
+        go_fast = True
+
+    else:
+        if dist_future < DIST_THRESHOLD:
+            go_fast = True
+        else:
+            diff_heading_mid, dist_mid = identify_corner(waypoints, i, MID_STEP)
+
+            if diff_heading_mid < TURN_THRESHOLD:
+                go_fast = True
+            else:
+                go_fast = False
+
+    # Slow down towards the end of the track
+    if progress > 80:
+        go_fast = False
+
+    if go_fast and speed > 2:
         reward += 0.5
 
-    if abs(steering_angle) > 10 and speed < 2:
-        reward += 0.5
+    elif not go_fast and speed < 2:
+        reward += 0.5    
       
     return float(reward)
--- waypoint_plotter.py
+++ waypoint_plotter.py
@@ -1,5 +1,47 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import math
+
+FUTURE_STEP = 7
+MID_STEP = 4
+TURN_THRESHOLD = 10    # degrees
+DIST_THRESHOLD = 1.2    # metres
+
+
+def identify_corner(waypoints, i, future_step):
+    closest_waypoints = [i-1, i]
+
+    # Identify next waypoint and further waypoint
+    point_prev = waypoints[closest_waypoints[0]]
+    point_next = waypoints[closest_waypoints[1]]
+    point_future = waypoints[min(len(waypoints)-1,closest_waypoints[1]+future_step)]
+
+    # Calculate headings to waypoints
+    heading_current = math.degrees(math.atan2(point_prev[1]-point_next[1], point_prev[0] - point_next[0]))
+    heading_future = math.degrees(math.atan2(point_prev[1]-point_future[1], point_prev[0]-point_future[0]))
+
+    # Circular Heading Calculations
+    if heading_current > heading_future and heading_current - heading_future > 180:
+        heading_offset = 180-heading_current
+        heading_current = -180
+        heading_future += heading_offset
+
+    elif heading_future > heading_current and heading_future - heading_current > 180:
+        heading_offset = 180-heading_future
+        heading_future = -180
+        heading_current += heading_offset
+
+    # Calculate the difference between the headings
+    diff_heading = abs(heading_current-heading_future)
+
+    # Calculate distance to waypoints
+    dist_future = np.linalg.norm([point_next[0]-point_future[0],point_next[1]-point_future[1]])  
+
+    return diff_heading, dist_future
+
+
+
+
 # Track Name from Tracks List
 track_name = "ChampionshipCup2019_track"
 # Location of tracks folder
@@ -8,10 +50,40 @@ absolute_path = "."
 waypoints = np.load("%s.npy" % (track_name))
 # Get number of waypoints
 print("Number of waypoints = " + str(waypoints.shape[0]))
+print("FUTURE_STEP: %d" % (FUTURE_STEP))
+print("MID_STEP: %d" % (MID_STEP))
+print("TURN_THRESHOLD: %d" % (TURN_THRESHOLD))
+print("DIST_THRESHOLD: %.1f" % (DIST_THRESHOLD))
+
+
+# Extract the x and y columns from the waypoints
+waypoints = waypoints[:,2:4]
+
 # Plot waypoints
-for i, point in enumerate(waypoints):
-    waypoint = (point[2], point[3])
-    plt.scatter(waypoint[0], waypoint[1])
-    print("Waypoint " + str(i) + ": " + str(waypoint))
+fast_colour = '#ff7f0e'
+slow_colour = '#1f77b4'
+bonus_fast_colour = '#ff460e'
+bonus_slow_colour = '#4e1fb4'
+for i in range(len(waypoints)):
+    diff_heading, dist_future = identify_corner(waypoints, i, FUTURE_STEP)
+
+    if diff_heading < TURN_THRESHOLD:
+        color = fast_colour
+    else:
+        if dist_future < DIST_THRESHOLD:
+            color = slow_colour
+        else:
+            diff_heading_mid, dist_mid = identify_corner(waypoints, i, MID_STEP)
+
+            if diff_heading_mid < TURN_THRESHOLD:
+                color = bonus_fast_colour
+            else:
+                color = slow_colour
+
+    if i/len(waypoints) > 0.80:
+        color = bonus_slow_colour
+
+    plt.scatter(waypoints[i][0], waypoints[i][1], c=color)
+    # print("Waypoint " + str(i) + ": " + str(waypoints[i]))
 
 plt.show()
