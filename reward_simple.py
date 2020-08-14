91ff70296e7ef503227d02bf0ae499b2472d5d4f
Matthew Suntup <matthewsuntup@gmail.com>
2020-08-14 14:55:47 +1000

    Adjustements and fixed speed incentive

--- reward_simple.py
+++ reward_simple.py
@@ -1,22 +1,26 @@
 def reward_function(params):
     '''
-    Our custom reward function
+    Reward function for AWS DeepRacer
+    Team: IndestruciRacer
+    Authors: Matthew Suntup, Georgia Markham, Ashan Abey
+    August 2020
     '''
     
     import math
     import numpy as np
     
-    # Params
+    # Parameters
     FUTURE_STEP = 7
     MID_STEP = 4
     TURN_THRESHOLD = 10     # degrees
     DIST_THRESHOLD = 1.2    # metres
     SPEED_THRESHOLD = 1.8   # m/s
-    PROGRESS_THRESHOLD = 80 # %
+    PROGRESS_THRESHOLD = 75 # %
+
 
     def identify_corner(waypoints, closest_waypoints, future_step):
 
-        # Identify next waypoint and further waypoint
+        # Identify next waypoint and a further waypoint
         point_prev = waypoints[closest_waypoints[0]]
         point_next = waypoints[closest_waypoints[1]]
         point_future = waypoints[min(len(waypoints)-1,closest_waypoints[1]+future_step)]
@@ -25,24 +29,47 @@ def reward_function(params):
         heading_current = math.degrees(math.atan2(point_prev[1]-point_next[1], point_prev[0] - point_next[0]))
         heading_future = math.degrees(math.atan2(point_prev[1]-point_future[1], point_prev[0]-point_future[0]))
 
-        # Circular Heading Calculations
-        if heading_current > heading_future and heading_current - heading_future > 180:
-            heading_offset = 180-heading_current
-            heading_current = -180
-            heading_future += heading_offset
-
-        elif heading_future > heading_current and heading_future - heading_current > 180:
-            heading_offset = 180-heading_future
-            heading_future = -180
-            heading_current += heading_offset
-
         # Calculate the difference between the headings
         diff_heading = abs(heading_current-heading_future)
 
-        # Calculate distance to waypoints
+        # Check we didn't choose the reflex angle
+        if diff_heading > 180:
+            diff_heading = 360 - diff_heading
+
+        # Calculate distance to further waypoint
         dist_future = np.linalg.norm([point_next[0]-point_future[0],point_next[1]-point_future[1]])  
 
         return diff_heading, dist_future
+
+
+    def select_speed(waypoints, closest_waypoints, future_step, mid_step, progress):
+
+        # Identify if a corner is in the future
+        diff_heading, dist_future = identify_corner(waypoints, closest_waypoints, future_step)
+
+        if diff_heading < TURN_THRESHOLD:
+            # If there's no corner encourage going faster
+            go_fast = True
+        else:
+            if dist_future < DIST_THRESHOLD:
+                # If there is a corner and it's close encourage going slower
+                go_fast = False
+            else:
+                # If the corner is far away, re-assess closer points
+                diff_heading_mid, dist_mid = identify_corner(waypoints, closest_waypoints, mid_step)
+
+                if diff_heading_mid < TURN_THRESHOLD:
+                    # If there's no corner encourage going faster
+                    go_fast = True
+                else:
+                    # If there is a corner and it's close encourage going slower
+                    go_fast = False
+
+        # Slow down towards the end of the track
+        if progress > PROGRESS_THRESHOLD:
+            go_fast = False
+
+        return go_fast
 
 
     # Read input parameters
@@ -52,7 +79,6 @@ def reward_function(params):
     is_offtrack = params['is_offtrack']
     progress = params['progress']
     speed = params['speed']
-    steering_angle = params['steering_angle']
     steps = params['steps']
     track_width = params['track_width']
     waypoints = params['waypoints']
@@ -64,7 +90,6 @@ def reward_function(params):
     
     # Give higher reward if the car is closer to centre line and vice versa
     # 0 if you're on edge of track, 1 if you're centre of track
-    
     if progress < PROGRESS_THRESHOLD:
         reward = 1 - (distance_from_center/(track_width/2))**(1/4) + progress/steps
 
@@ -72,29 +97,9 @@ def reward_function(params):
         # Towards the end prioritise sticking to the centre of the track
         reward = 1 - (distance_from_center/(track_width/2))
 
+    go_fast = select_speed(waypoints, closest_waypoints, FUTURE_STEP, MID_STEP, progress)
 
-    diff_heading, dist_future = identify_corner(waypoints, closest_waypoints, FUTURE_STEP)
-
-    go_fast = True
-
-    if diff_heading < TURN_THRESHOLD:
-        go_fast = True
-
-    else:
-        if dist_future < DIST_THRESHOLD:
-            go_fast = True
-        else:
-            diff_heading_mid, dist_mid = identify_corner(waypoints, closest_waypoints, MID_STEP)
-
-            if diff_heading_mid < TURN_THRESHOLD:
-                go_fast = True
-            else:
-                go_fast = False
-
-    # Slow down towards the end of the track
-    if progress > PROGRESS_THRESHOLD:
-        go_fast = False
-
+    # Implement speed incentive
     if go_fast and speed > SPEED_THRESHOLD:
         reward += 0.5
 
--- waypoint_plotter.py
+++ waypoint_plotter.py
@@ -6,11 +6,11 @@ FUTURE_STEP = 7
 MID_STEP = 4
 TURN_THRESHOLD = 10    # degrees
 DIST_THRESHOLD = 1.2    # metres
+PROGRESS_THRESHOLD = 75 # %
 
 
-def identify_corner(waypoints, i, future_step):
-    closest_waypoints = [i-1, i]
-
+def identify_corner(waypoints, closest_waypoints, future_step):
+    
     # Identify next waypoint and further waypoint
     point_prev = waypoints[closest_waypoints[0]]
     point_next = waypoints[closest_waypoints[1]]
@@ -40,6 +40,34 @@ def identify_corner(waypoints, i, future_step):
     return diff_heading, dist_future
 
 
+    def select_speed(waypoints, closest_waypoints, future_step, mid_step, progress):
+
+        # Identify if a corner is in the future
+        diff_heading, dist_future = identify_corner(waypoints, closest_waypoints, future_step)
+
+        if diff_heading < TURN_THRESHOLD:
+            # If there's no corner encourage going faster
+            go_fast = True
+        else:
+            if dist_future < DIST_THRESHOLD:
+                # If there is a corner and it's close encourage going slower
+                go_fast = False
+            else:
+                # If the corner is far away, re-assess closer points
+                diff_heading_mid, dist_mid = identify_corner(waypoints, closest_waypoints, mid_step)
+
+                if diff_heading_mid < TURN_THRESHOLD:
+                    # If there's no corner encourage going faster
+                    go_fast = True
+                else:
+                    # If there is a corner and it's close encourage going slower
+                    go_fast = False
+
+        # Slow down towards the end of the track
+        if progress > PROGRESS_THRESHOLD:
+            go_fast = False
+
+        return go_fast
 
 
 # Track Name from Tracks List
@@ -65,22 +93,33 @@ slow_colour = '#1f77b4'
 bonus_fast_colour = '#ff460e'
 bonus_slow_colour = '#4e1fb4'
 for i in range(len(waypoints)):
-    diff_heading, dist_future = identify_corner(waypoints, i, FUTURE_STEP)
+
+    # Simulate input parameters in race
+    closest_waypoints = [i-1, i]
+    progress = i/len(waypoints)
+
+
+    diff_heading, dist_future = identify_corner(waypoints, closest_waypoints, FUTURE_STEP)
 
     if diff_heading < TURN_THRESHOLD:
+        # If there's no corner encourage going faster
         color = fast_colour
     else:
         if dist_future < DIST_THRESHOLD:
+            # If there is a corner and it's close encourage going slower
             color = slow_colour
         else:
-            diff_heading_mid, dist_mid = identify_corner(waypoints, i, MID_STEP)
+            # If the corner is far away, re-assess closer points
+            diff_heading_mid, dist_mid = identify_corner(waypoints, closest_waypoints, MID_STEP)
 
             if diff_heading_mid < TURN_THRESHOLD:
+                # If there's no corner encourage going faster
                 color = bonus_fast_colour
             else:
+                # If there is a corner and it's close encourage going slower
                 color = slow_colour
 
-    if i/len(waypoints) > 0.80:
+    if i/len(waypoints) > PROGRESS_THRESHOLD/100:
         color = bonus_slow_colour
 
     plt.scatter(waypoints[i][0], waypoints[i][1], c=color)
